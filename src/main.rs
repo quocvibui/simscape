7b7fa944f00ad184931012db384969e6107e2327
Quoc Bui <buiviquoc@gmail.com>
2026-02-24 18:54:08 -0500

    Added simple button and quick test with egui

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,47 @@
 extern crate nannou;
 use nannou::prelude::*;
+use nannou_egui::{self, egui, Egui};
 
 fn main() {
     nannou::app(model)
         .update(update)
-        .simple_window(view)
         .run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+struct Model {
+    egui: Egui, // egui state lives here
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+fn model(app: &App) -> Model {
+    let window_id = app
+        .new_window()
+        .view(view)
+        .raw_event(raw_window_event)
+        .build()
+        .unwrap();
+    let window = app.window(window_id).unwrap();
+
+    Model {
+        egui: Egui::from_window(&window)
+    }
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame){
+fn update(_app: &App, model: &mut Model, update: Update) {
+    model.egui.set_elapsed_time(update.since_start);
+    let ctx = model.egui.begin_frame();
+    egui::Window::new("My Panel").show(&ctx, |ui| {
+        if ui.button("Click me").clicked() {
+            // do something here
+        }
+    });
+}
+
+fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    model.egui.handle_raw_event(event);
+}
+
+fn view(_app: &App, model: &Model, frame: Frame){
     frame.clear(PURPLE);
+    model.egui.draw_to_frame(&frame).unwrap();
 }
 
